646544490d473537b23ee71656a9e8bda0aaeca6
Michael Hansen <modality2@gmail.com>
2013-03-24 19:38:45 -0400

    add pausing, reformat squarepig, prevent squarepig from adding an obj twice

--- js/controllers/game_controller.js
+++ js/controllers/game_controller.js
@@ -9,6 +9,7 @@ JJ.GameController = function() {
 
   this.grid = new pig.Grid(0, 0, 640, 480, 40, 40);
   this.game_map = new JJ.GameMap(this.grid);
+  this.paused = false;
 
   var janet = new JJ.Player(320, 240);
   var cursor = new JJ.Cursor(0, 0);
@@ -20,27 +21,36 @@ JJ.GameController = function() {
 
   var modeOptions = {};
 
+  this.update = function(dtime) {
+    if(!this.paused) {
+      this._update(dtime);
+    }
+  };
+
   this.changeGameMode = function(mode) {
     console.log('mode', mode);
     this[mode+'Mode']();
   };
 
   this.playMode = function() {
+    this.paused = false;
     pig.world.remove(cursor);
     pig.world.add(janet);
   };
 
   this.editMode = function() {
+    this.paused = false;
     pig.world.remove(janet);
     pig.world.add(cursor);
   };
+
+  this.rulesMode = function() {
+    this.paused = true;
+  }
 
   cursor.mouseDown = function() {
     pig.world.game_map.setTile(Math.floor(pig.mouse.x / JJ.Constants.TILE_W),
                                Math.floor(pig.mouse.y / JJ.Constants.TILE_H),
                                1);
   }
-
-  this.rulesMode = function() {
-  };
 };
